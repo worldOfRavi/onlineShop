d33b6ca2e7c7cf749e743e5e52a4a6d4896c6248
worldOfRavi <rajendra20023@gmail.com>
2024-12-23 10:50:48 -0500

    logout function added in backend

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -82,6 +82,23 @@ class AuthController {
             
         }
     }
+
+    // logut function
+    static async authLogout(req, res){
+        try {
+            res.clearCookie("token").status(200).json({
+                success:true,
+                message:"Logged out successfully"
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                success:false,
+                message:"Something went wrong"
+            })
+            
+        }
+    }
 }
 
 export default AuthController;
