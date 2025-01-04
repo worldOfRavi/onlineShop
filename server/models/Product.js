4f6dcf1fabc1377d30940173a1baef640f261497
worldOfRavi <rajendra20023@gmail.com>
2025-01-04 16:22:09 -0500

    created product model

---
+++ server/models/Product.js
@@ -0,0 +1,26 @@
+// image: null,
+//   title: "",
+//   description: "",
+//   category: "",
+//   brand: "",
+//   price: "",
+//   salePrice: "",
+//   totalStock: ""
+
+import { Schema, model} from 'mongoose';
+
+const productSchema = new Schema({
+   image : String,
+   title : String,
+   description : String,
+   category : String,
+   brand : String,
+   price : Number,
+   salePrice : Number,
+   totalStock : Number
+}, {timestamps : true});
+
+
+const Product =  model("Product", productSchema);
+
+export default Product;
