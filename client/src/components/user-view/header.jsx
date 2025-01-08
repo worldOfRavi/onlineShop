6135136e69845dd001cff678b16105841f47ed4a
worldOfRavi <rajendra20023@gmail.com>
2025-01-07 23:32:49 -0500

    changes in use view header

--- client/src/components/user-view/header.jsx
+++ client/src/components/user-view/header.jsx
@@ -1,10 +1,18 @@
+import { HousePlug } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UserHeader = () => {
   return (
-    <div>
-      User view header
-    </div>
+    <header className='sticky top-0 z-40 border-b bg-background'>
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/user/home" className='flex items-center gap-2'>
+            <HousePlug className='w-6 h-6' />
+            <span className='font-bold'>Ecommerce</span>
+        </Link>
+         
+      </div>
+    </header>
   )
 }
 
