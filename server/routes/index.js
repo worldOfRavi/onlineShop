5019f726561cfb5701ec6fa4e93570888a12d013
worldOfRavi <rajendra20023@gmail.com>
2025-01-09 19:51:29 -0500

    product controller and router for user are done in backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,11 @@ import adminProductRouter from "./admin/products-routes.js";
 import userProductRouter from "./user/products-route.js"
 const router  = express.Router();
 
+// auht route
 router.use("/api/auth",authRouter);
+// admin route
 router.use("/api/admin/products",adminProductRouter);
+// user route
 router.use("/api/user/products",userProductRouter);
 
 export default router;
