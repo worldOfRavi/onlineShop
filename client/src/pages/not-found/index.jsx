65a88c09761b016a2af999174879da40fddc04e6
worldOfRavi <rajendra20023@gmail.com>
2025-02-18 01:15:26 -0500

    page not found is done

--- client/src/pages/not-found/index.jsx
+++ client/src/pages/not-found/index.jsx
@@ -1,9 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import notFound from "../../assets/404.svg"
+import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      404 Error, page does not found...
+      <Card>
+        <CardContent asChild>
+            <img className='w-[90%] mx-auto object-cover' src={notFound} alt="404 error" />
+            <div className="text-center">
+            <Button onClick={()=>navigate("/user/home")} className="bg-red-500">Go to Home Page</Button>
+            </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
